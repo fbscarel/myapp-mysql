23aa233caa8cddbb125351d8c2d63cc8509e2cd6
Felipe Scarel <fbscarel@gmail.com>
2020-09-20 00:29:22 -0300

    v1

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
 from flask import Flask
 from flask import render_template
-import pymysql.cursors
 import argparse
+import mysql.connector
 import os
 import socket
 
 app = Flask(__name__)
 
+def argtest (cli, env, default):
+  if cli:
+    return cli
+  elif env:
+    return env
+  else:
+    return default
+
 @app.route('/')
 def hello():
-  return render_template('index.html', hostname=socket.gethostname(), dbhost=DBHOST, dbuser=DBUSER, dbpass=DBPASS)
+  try:
+    mysql.connector.connect(host=DBHOST, database=DATABASE, user=DBUSER, password=DBPASS)
+    status = SUCCESS
+    color = '#9eff9a'
+  except Exception as e:
+    msg = str(e)
+    status = FAILURE
+    color = '#ff9a9e'
+
+  return render_template('index.html', hostname=socket.gethostname(), dbhost=DBHOST, database=DATABASE, dbuser=DBUSER, dbpass=DBPASS, status=status, color=color)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-h', '--host', required=False)
-  parser.add_argument('-u', '--user', required=False)
-  parser.add_argument('-p', '--pass', required=False)
+  parser = argparse.ArgumentParser(add_help=False)
+  parser.add_argument('-h', '--hostname', required=False)
+  parser.add_argument('-d', '--database', required=False)
+  parser.add_argument('-u', '--username', required=False)
+  parser.add_argument('-p', '--password', required=False)
   args = parser.parse_args()
 
-#fixthis
-  if os.environ.get('DBHOST'):
-    DBHOST = os.environ.get('DBHOST')
-  else:
-    DBHOST = args.dbhost
-
-  if os.environ.get('DBHOST'):
-    DBHOST = os.environ.get('DBHOST')
-  else:
-    DBHOST = args.dbhost
-
-  if os.environ.get('DBHOST'):
-    DBHOST = os.environ.get('DBHOST')
-  else:
-    DBHOST = args.dbhost
+  DBHOST   = argtest (args.hostname, os.environ.get('DBHOST'),   'localhost')
+  DBUSER   = argtest (args.username, os.environ.get('DBUSER'),   'root')
+  DBPASS   = argtest (args.password, os.environ.get('DBPASS'),   '')
+  DATABASE = argtest (args.database, os.environ.get('DATABASE'), 'mysql')
 
   app.run(host='0.0.0.0', port=80)
 
